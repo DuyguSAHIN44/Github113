c3d20de8aba367a81b26cd267b4a6321c4f53bf1
DuyguSAHIN44 <duygusahin190344@gmail.com>
2023-03-27 18:39:35 +0300

    ikinci version

--- src/github_İlkDers.java
+++ src/github_İlkDers.java
@@ -1,2 +1,7 @@
 public class github_İlkDers {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+        System.out.println("*******************");
+        System.out.println("Have ");
+    }
 }
